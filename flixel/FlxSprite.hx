3a4234f745001201a3eeeb9a6b9573ef3bf1f5f5
glowsoony <84847356+glowsoony@users.noreply.github.com>
2024-08-18 21:34:50 -0400

    Update FlxSprite.hx

--- flixel/FlxSprite.hx
+++ flixel/FlxSprite.hx
@@ -807,6 +807,9 @@ class FlxSprite extends FlxObject
 		if (dirty) // rarely
 			calcFrame(useFramePixels);
 
+		if (shader != null && shader is FlxGraphicsShader)
+			shader.setCamSize(_frame.frame.x, _frame.frame.y, _frame.frame.width, _frame.frame.height);
+
 		for (camera in getCamerasLegacy())
 		{
 			if (!camera.visible || !camera.exists || !isOnScreen(camera))
