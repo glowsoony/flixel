222a4c1455ce2430c28b4d279bbf3d43f5ef6349
glowsoony <84847356+glowsoony@users.noreply.github.com>
2024-09-26 09:09:03 -0400

    Update FlxGraphicsShader.hx

--- flixel/graphics/tile/FlxGraphicsShader.hx
+++ flixel/graphics/tile/FlxGraphicsShader.hx
@@ -23,7 +23,7 @@ class FlxGraphicsShader extends GraphicsShader
 				openfl_ColorOffsetv = colorOffset / 255.0;
 				openfl_ColorMultiplierv = colorMultiplier;
 			}
-		}", true)
+		}")
 	@:glFragmentHeader("
 		uniform bool hasTransform;
 		uniform bool hasColorTransform;
@@ -75,14 +75,14 @@ class FlxGraphicsShader extends GraphicsShader
 		vec4 textureCam(sampler2D bitmap, vec2 pos) {
 			return flixel_texture2D(bitmap, camToOg(pos));
 		}
-		", true)
+		")
 	@:glFragmentSource("
 		#pragma header
 		
 		void main(void)
 		{
 			gl_FragColor = flixel_texture2D(bitmap, openfl_TextureCoordv);
-		}", true)
+		}")
 	public function new()
 	{
 		super();
